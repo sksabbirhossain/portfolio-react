9c54b9173757245c4f25af0691917237e37863f8
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-12-04 23:53:08 +0600

    created a protfolio section

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,11 +1,114 @@
-import React from 'react';
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { Autoplay, Navigation, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+// import slider1 from "../../images/slider/slider1.png";
+// import slider2 from "../../images/slider/slider2.png";
+// import slider3 from "../../images/slider/slider3.png";
+import styles from "./Portfolio.module.css";
 
 const Portfolio = () => {
-    return (
-        <section>
-            <h3>Portfolio</h3>
-        </section>
-    );
+  return (
+    <section id="portfolio">
+      <div className="container">
+        <div className="heading">
+          <h2>Portfolio</h2>
+          <span>Most recent Work</span>
+        </div>
+      </div>
+      <Swiper
+        loop={true}
+        autoplay={{
+          delay: 3000,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className={`${styles.sliderItems}`}>
+            <div className="container">
+              <div className="row d-flex align-items-center">
+                <div className="col-md-6">
+                  <div className="">
+                    <img
+                      src="https://cdn.dribbble.com/users/1250068/screenshots/9174002/media/5cf2aefe878528cb59c57b5f91f70ed6.png?compress=1&resize=400x300&vertical=top"
+                      alt=""
+                      className="img-fluid rounded"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className=" pt-4 pt-md-0">
+                    <h4 className={styles.sliderTitle}>
+                      Daily Food - food dalivery services
+                    </h4>
+                    <p>
+                      I am providing food dalivery services. I cook all my food
+                      at my home. I try to keep my diet very good . i also
+                      deliver the ordered food the order's home. and i didn't
+                      charge extra for that.
+                    </p>
+                    <div className="mt-4">
+                      <a
+                        href="https://daily-food-1dafd.web.app/"
+                        className="mainBtn"
+                      >
+                        View <FaArrowRight />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={`${styles.sliderItems}`}>
+            <div className="container">
+              <div className="row d-flex align-items-center">
+                <div className="col-md-6">
+                  <div className="">
+                    <img
+                      src="https://cdn.dribbble.com/users/1250068/screenshots/9174002/media/5cf2aefe878528cb59c57b5f91f70ed6.png?compress=1&resize=400x300&vertical=top"
+                      alt=""
+                      className="img-fluid rounded"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className=" pt-4 pt-md-0">
+                    <h4 className={styles.sliderTitle}>
+                      E-Learning - for build your knowlage
+                    </h4>
+                    <p>
+                      reactjs is used to create Client Side. and express js to
+                      used to Server Side. and used react-bootstrap only for the
+                      header. also, used react-router-dom(v6) for routing.
+                      design all pages by bootstrap 5.
+                    </p>
+                    <div className="mt-4">
+                      <a
+                        href="https://e-learning-91ee1.web.app/"
+                        className="mainBtn"
+                      >
+                        View <FaArrowRight />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
 };
 
 export default Portfolio;
